67247ff1bceaa905cea646c2431e7dc94c4546b2
ming920 <rkfsky920@gmail.com>
2026-03-04 02:25:21 +0900

    build: 초기 환경 구축

---
+++ src/test/java/com/yu/audit/AuditApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yu.audit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuditApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
