4143df2b599ba7b95eeb5961a7b8622ce2d0b523
ming920 <rkfsky920@gmail.com>
2026-03-06 02:20:11 +0900

    feat: Slack 연동

---
+++ src/main/java/com/yu/audit/controller/LogTestController.java
@@ -0,0 +1,15 @@
+package com.yu.audit.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class LogTestController {
+
+    @GetMapping("/error")
+    public String errorTest() {
+        throw new RuntimeException("Slack ERROR 테스트입니다.");
+    }
+}
